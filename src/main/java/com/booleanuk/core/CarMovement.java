547fa2c8e71236d7aa6a255330617edd060f8350
Aidan van Geest <aidanvangeest@gmail.com>
2023-05-11 13:03:18 +0200

    move forward tests written

--- src/main/java/com/booleanuk/core/CarMovement.java
+++ src/main/java/com/booleanuk/core/CarMovement.java
@@ -1,4 +1,12 @@
 package com.booleanuk.core;
 
 public class CarMovement {
+    Car car;
+
+    public CarMovement(Car car) {
+        this.car = car;
+    }
+
+
+
 }
--- src/test/java/com/booleanuk/core/CarMovementTest.java
+++ src/test/java/com/booleanuk/core/CarMovementTest.java
@@ -1,4 +1,19 @@
 package com.booleanuk.core;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class CarMovementTest {
+
+    @Test
+    public void testMoveForward() {
+        Car car1 = new Car("red", true, 5);
+        CarMovement carMovement = new CarMovement(car1);
+
+        //Successful test
+        Assertions.assertTrue(carMovement.moveForward());
+
+        //Failure test
+        Assertions.assertFalse(carMovement.moveForward());
+    }
 }
