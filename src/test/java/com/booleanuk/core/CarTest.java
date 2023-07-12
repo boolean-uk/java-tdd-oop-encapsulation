12ae8663fda746cfcf8f4af2ea1411fe4de3d8bd
himok <himok.pk@gmail.com>
2023-07-12 11:06:46 +0200

    Adding moveTest

--- src/test/java/com/booleanuk/core/CarTest.java
+++ src/test/java/com/booleanuk/core/CarTest.java
@@ -1,5 +1,15 @@
 package com.booleanuk.core;
 
-public class CarTest {
+import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class CarTest {
+    @Test
+    public void moveTest() {
+        Car car = new Car();
+        assertEquals("Car moved forward by 10km", car.move("f", 10));
+        assertEquals("Car moved backwards by 5km", car.move("b", 5));
+        assertEquals("Car run out of battery", car.move("f", 30));
+    }
 }
