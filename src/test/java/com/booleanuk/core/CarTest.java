e384fd1f73531a29e6f53b57bd0fdce118bef59e
Kinga <kinga.hernet@talan.com>
2023-07-12 11:09:17 +0200

    Added 3 tests to CarTest.java

--- src/test/java/com/booleanuk/core/CarTest.java
+++ src/test/java/com/booleanuk/core/CarTest.java
@@ -1,4 +1,49 @@
 package com.booleanuk.core;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class CarTest {
+    @Test
+    public void testMoveForward() {
+        remote.setType("Advanced");
+        car.setRemote(remote);
+        battery.setType("Rechargeable");
+        battery.setLevel(100);
+        car.setBattery(battery);
+        car.turn("Forward");
+        car.move(10);
+        Assertions.assertEquals(10, car.getPosition());
+        Assertions.assertEquals("Forward", car.getDirection());
+        Assertions.assertEquals(99, car.getBattery().getLevel());
+    }
+
+    @Test
+    public void testMoveBackward() {
+        remote.setType("Advanced");
+        car.setRemote(remote);
+        battery.setType("Rechargeable");
+        battery.setLevel(100);
+        car.setBattery(battery);
+        car.turn("Backward");
+        car.move(10);
+        Assertions.assertEquals(-10, car.getPosition());
+        Assertions.assertEquals("Backward", car.getDirection());
+        Assertions.assertEquals(99, car.getBattery().getLevel());
+    }
+
+    @Test
+    public void testStopMoving() {
+        remote.setType("Advanced");
+        car.setRemote(remote);
+        battery.setType("Rechargeable");
+        battery.setLevel(100);
+        car.setBattery(battery);
+        car.turn("Forward");
+        car.move(10);
+        car.stop();
+        Assertions.assertEquals(10, car.getPosition());
+        Assertions.assertEquals("Forward", car.getDirection());
+        Assertions.assertEquals(99, car.getBattery().getLevel());
+    }
 }
