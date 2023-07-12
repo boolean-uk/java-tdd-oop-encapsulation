7ad07df73a94fad6ae8e1ea7370a56ca901d8218
Jakub Pawlowski <jakub.pawloski@talan.com>
2023-07-12 11:00:57 +0200

    Code red

---
+++ src/main/java/com/booleanuk/core/RemoteType.java
@@ -0,0 +1,6 @@
+package com.booleanuk.core;
+
+public enum RemoteType {
+    SIMPLE,
+    ADVANCED
+}
---
+++ src/test/java/com/booleanuk/core/CarTest.java
@@ -0,0 +1,72 @@
+package com.booleanuk.core;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class CarTest {
+    Car car;
+
+    @BeforeEach
+    car = new Car("blue", BatteryType.RECHARGABLE, 50, RemoteType.SIMPLE);
+
+    @Test
+    public void setColourTest(){
+        car.setColour("red");
+        Assertions.assertEquals("red", car.getColour());
+    }
+
+    @Test
+    public void setBatteryTest(){
+        car.setBattery(BatteryType.DISPOSABLE);
+        Assertions.assertEquals(BatteryType.DISPOSABLE, car.getBattery());
+    }
+
+    @Test
+    public void setRemoteTest(){
+        car.setRemote(RemoteType.ADVANCED);
+        Assertions.assertEquals(RemoteType.ADVANCED, car.getRemote());
+    }
+
+    @Test
+    public void getBatteryPercentageTest(){
+        Assertions.assertEquals(50, car.getBatteryPercentage());
+    }
+
+    @Test
+    public void moveForwardTest(){
+        car.moveForward(5);
+        Assertions.assertEquals(5, car.getDistance());
+    }
+
+    @Test
+    public void moveBackwardTest(){
+        car.moveBackward(5);
+        Assertions.assertEquals(5, car.getDistance());
+    }
+
+    @Test
+    public void moveRandomlyTest(){
+        car.moveBackward(5);
+        car.moveForward(3);
+        car.moveBackward(10);
+        Assertions.assertEquals(18, car.getDistance());
+    }
+
+    @Test
+    public void stopTest(){
+        Assertions.assertEquals(true, car.stop());
+    }
+
+    @Test
+    public void turnLeftTest(){
+        car.turnLeft();
+        Assertions.assertEquals("Left", car.getDirection());
+    }
+
+    @Test
+    public void turnRightTest(){
+        car.turnRight();
+        Assertions.assertEquals("Right", car.getDirection());
+    }
+}
