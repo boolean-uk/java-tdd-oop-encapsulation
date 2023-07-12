84d2ed19039180bebadfc7ef82c9b3db4c44a85e
Kamil Kulak <kamil.kulak@talan.com>
2023-07-12 11:15:16 +0200

    Red stage for battery

---
+++ src/test/java/com/booleanuk/core/BatteryTest.java
@@ -0,0 +1,26 @@
+package com.booleanuk.core;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class BatteryTest {
+    Battery battery;
+
+    @BeforeEach
+    public void setup() {
+        battery = new Battery();
+    }
+    @Test
+    public void BatteryGetersAndSettersTest() {
+
+        Assertions.assertEquals("rechargable", battery.getType());
+        Assertions.assertEquals(88, battery.getPercentage());
+
+        battery.setType("disposable");
+        battery.setPercentage(44);
+
+        Assertions.assertEquals("disposable", battery.getType());
+        Assertions.assertEquals(44, battery.getPercentage());
+    }
+}
