f0e9294114aa857b98ab3817c5298e259a2bd542
Szkaradd <szkaradekmikolaj@gmail.com>
2023-07-12 10:59:21 +0200

    created classes

---
+++ src/test/java/com/booleanuk/core/BatteryTest.java
@@ -0,0 +1,16 @@
+package com.booleanuk.core;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+public class BatteryTest {
+    @BeforeAll
+    public static void setUp() {
+        Battery battery = new Battery("Duracell");
+    }
+
+    @Test
+    public static void testGetPercentage() {
+        
+    }
+}
