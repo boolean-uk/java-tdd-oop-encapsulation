265de1da31b66dde084998f59daa171797e2746c
jakublaba <jakub.maciej.laba@gmail.com>
2023-07-12 10:58:52 +0200

    Refactor RcCar

--- src/main/java/com/booleanuk/core/RcCar.java
+++ src/main/java/com/booleanuk/core/RcCar.java
@@ -2,12 +2,14 @@ package com.booleanuk.core;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Getter;
 import lombok.Setter;
 
 import java.awt.*;
 
 @AllArgsConstructor
 @Builder
+@Getter
 public class RcCar {
     private static final int BATTERY_DRAIN = 1;
 
@@ -15,13 +17,19 @@ public class RcCar {
     @Setter
     private Color color;
     @Setter
-    private BatteryType batteries;
+    private BatteryType batteryType;
     private final RcType rcType;
-    private short batteryPercentage;
+    private int batteryPercentage;
     @Setter
     private Direction direction;
 
     public void move(double distance) {
+        this.distanceTravelled += distance;
         this.batteryPercentage -= BATTERY_DRAIN * distance;
+    }
+
+    public void replaceBatteries(BatteryType type) {
+        this.batteryType = type;
+        this.batteryPercentage = 100;
     }
 }
