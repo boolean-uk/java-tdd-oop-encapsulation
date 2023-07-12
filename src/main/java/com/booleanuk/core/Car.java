3791520ee098a028bb4da50482a69deefba4872a
himok <himok.pk@gmail.com>
2023-07-12 11:18:43 +0200

    Adding move method

--- src/main/java/com/booleanuk/core/Car.java
+++ src/main/java/com/booleanuk/core/Car.java
@@ -1,4 +1,23 @@
 package com.booleanuk.core;
 
 public class Car {
+    private int batteryLevel;
+    public Car() {
+        this.batteryLevel = 100;
+    }
+
+    public String move(char direction, int distance) {
+        if(this.batteryLevel == 0)
+            return "Car is out of fuel";
+        if(direction != 'f' && direction != 'b')
+            return "Invalid direction";
+        String fullDirection = direction == 'f' ? "forward" : "backwards";
+        if(this.batteryLevel < distance * 5) {
+            int moved = this.batteryLevel / 5;
+            this.batteryLevel = 0;
+            return "Car moved " + fullDirection + " by " + moved + "km";
+        }
+        this.batteryLevel -= distance * 5;
+        return "Car moved " + fullDirection + " by " + distance + "km";
+    }
 }
--- src/test/java/com/booleanuk/core/CarTest.java
+++ src/test/java/com/booleanuk/core/CarTest.java
@@ -8,8 +8,9 @@ public class CarTest {
     @Test
     public void moveTest() {
         Car car = new Car();
-        assertEquals("Car moved forward by 10km", car.move("f", 10));
-        assertEquals("Car moved backwards by 5km", car.move("b", 5));
-        assertEquals("Car run out of battery", car.move("f", 30));
+        assertEquals("Car moved forward by 10km", car.move('f', 10));
+        assertEquals("Car moved backwards by 5km", car.move('b', 5));
+        assertEquals("Car moved forward by 5km", car.move('f', 30));
+        assertEquals("Car is out of fuel", car.move('f', 20));
     }
 }
