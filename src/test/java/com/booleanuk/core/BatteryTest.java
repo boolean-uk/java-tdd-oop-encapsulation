9a326772d67384c847fc92e00d16251bc2d45324
Szkaradd <szkaradekmikolaj@gmail.com>
2023-07-12 11:05:28 +0200

    red tests for battery added

--- src/test/java/com/booleanuk/core/BatteryTest.java
+++ src/test/java/com/booleanuk/core/BatteryTest.java
@@ -1,16 +1,24 @@
 package com.booleanuk.core;
 
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class BatteryTest {
-    @BeforeAll
-    public static void setUp() {
-        Battery battery = new Battery("Duracell");
+    private Battery battery;
+
+    @BeforeEach
+    public void setUp() {
+        battery = new Battery("Duracell");
     }
 
     @Test
-    public static void testGetPercentage() {
-        
+    public static void testGetRemainingPercentageShouldReturn100() {
+        // Execute
+        int actual = battery.getRemainingPercentage();
+
+        // Verify
+        int expected = 100;
+        assertEquals(expected, actual);
     }
 }
