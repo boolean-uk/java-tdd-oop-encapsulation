f193781a9dadac1530302b2683fcb84a6f468369
PawelCagara <pawel.cagara@talan.com>
2023-07-12 11:10:19 +0200

    getBatteryPercentageTest() created

--- src/main/java/com/booleanuk/core/Car.java
+++ src/main/java/com/booleanuk/core/Car.java
@@ -1,4 +1,15 @@
 package com.booleanuk.core;
 
 public class Car {
+
+    private String colour;
+    private Battery battery;
+    private RemoteControl remoteControl;
+
+    public Car(String colour, Battery battery, RemoteControl remoteControl) {
+        this.colour = colour;
+        this.battery = battery;
+        this.remoteControl = remoteControl;
+    }
+
 }
---
+++ src/test/java/com/booleanuk/core/CarTest.java
@@ -0,0 +1,21 @@
+package com.booleanuk.core;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class CarTest {
+
+    Car car;
+
+    public CarTest() {
+        Battery battery = new Battery("rechargable", 50);
+        RemoteControl remoteControl = new RemoteControl("advanced");
+        this.car = new Car("red", battery, remoteControl );
+    }
+
+    @Test
+    public void getBatteryPercentageTest() {
+        Assertions.assertEquals(50, car.getBatteryPercentage());
+        Assertions.assertNotEquals(70, car.getBatteryPercentage());
+    }
+}
