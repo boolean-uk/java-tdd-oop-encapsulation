2bd68b500cba4b20be9c84b090bb9a4e8f6fcd05
Kornel <korneldabrowski@gmail.com>
2023-07-12 11:00:56 +0200

    implementing car color

--- src/main/java/com/booleanuk/core/Car.java
+++ src/main/java/com/booleanuk/core/Car.java
@@ -2,4 +2,17 @@ package com.booleanuk.core;
 
 public class Car {
 
+    private String color;
+
+    public Car() {
+        this.color = "white";
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
 }
