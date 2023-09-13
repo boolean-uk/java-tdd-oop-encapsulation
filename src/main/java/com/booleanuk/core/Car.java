f764be2c55180f625351151e62a9efd88d4cb557
VeroniqueDM <veronika.dmiteva@gmail.com>
2023-09-13 13:30:05 +0200

    modified tests and classes structure

--- src/main/java/com/booleanuk/core/Car.java
+++ src/main/java/com/booleanuk/core/Car.java
@@ -1,4 +1,65 @@
 package com.booleanuk.core;
 
 public class Car {
+    private String color;
+    private Battery battery;
+    private boolean hasAdvancedControl;
+    private String direction;
+    private boolean isMoving;
+
+
+
+    public Car(String color, Battery battery, boolean hasAdvancedControl) {
+        this.setColor(color);
+        this.setBattery(battery);
+        this.setHasAdvancedControl(hasAdvancedControl);
+        this.direction = "";
+        this.isMoving = false;
+    }
+    public String getColor() {
+        return color;
+    }
+
+    public boolean setColor(String color) {
+        if (color.isBlank()) {
+            return false;
+        }
+        this.color = color;
+        return true;
+    }
+
+    public Battery getBattery() {
+        return battery;
+    }
+
+    public void setBattery(Battery battery) {
+        this.battery = battery;
+    }
+    public void replaceBattery(String type) {
+        this.battery.setType(type);
+    }
+
+    public boolean hasAdvancedControl() {
+        return hasAdvancedControl;
+    }
+
+    public void setHasAdvancedControl(boolean hasAdvancedControl) {
+        this.hasAdvancedControl = hasAdvancedControl;
+    }
+
+    public String getDirection() {
+        return direction;
+    }
+
+    public void setDirection(String direction) {
+        this.direction = direction;
+    }
+
+    public boolean isMoving() {
+        return isMoving;
+    }
+
+    public void setMoving(boolean moving) {
+        isMoving = moving;
+    }
 }
--- src/test/java/com/booleanuk/core/CarTest.java
+++ src/test/java/com/booleanuk/core/CarTest.java
@@ -6,10 +6,29 @@ import org.junit.jupiter.api.Test;
 public class CarTest {
 
     @Test
-    public void testAddTaskSuccessful() {
-        Car car = new Car();
+    public void testSetColor() {
+        Battery battery = new Battery("disposable");
+        Car car = new Car("", battery, false);
+        Assertions.assertNull(car.getColor());
         Assertions.assertTrue(car.setColor("Black"));
         Assertions.assertFalse(car.setColor("\n\r\t"));
     }
 
+    @Test
+    public void testSetBattery() {
+        Battery battery = new Battery("disposable");
+        Car car = new Car("Red", battery, false);
+        Assertions.assertEquals("disposable",car.getBattery().getType());
+        car.replaceBattery("rechargeable");
+       Assertions.assertEquals("rechargeable",car.getBattery().getType());
+    }
+    @Test
+    public void testSetControl() {
+        Battery battery = new Battery("disposable");
+        Car car = new Car("Red", battery, false);
+        Assertions.assertFalse(car.hasAdvancedControl());
+        car.setHasAdvancedControl(true);
+        Assertions.assertTrue(car.hasAdvancedControl());
+    }
+   
 }
