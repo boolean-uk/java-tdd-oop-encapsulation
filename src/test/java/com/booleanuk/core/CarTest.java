31917eaac53da056316676a6c823e20bfc175c55
VeroniqueDM <veronika.dmiteva@gmail.com>
2023-09-13 16:55:25 +0200

    added more tests

--- src/test/java/com/booleanuk/core/CarTest.java
+++ src/test/java/com/booleanuk/core/CarTest.java
@@ -18,10 +18,11 @@ public class CarTest {
     public void testSetBattery() {
         Battery battery = new Battery("disposable");
         Car car = new Car("Red", battery, false);
-        Assertions.assertEquals("disposable",car.getBattery().getType());
+        Assertions.assertEquals("disposable", car.getBattery().getType());
         car.replaceBattery("rechargeable");
-       Assertions.assertEquals("rechargeable",car.getBattery().getType());
+        Assertions.assertEquals("rechargeable", car.getBattery().getType());
     }
+
     @Test
     public void testSetControl() {
         Battery battery = new Battery("disposable");
@@ -30,23 +31,32 @@ public class CarTest {
         car.setHasAdvancedControl(true);
         Assertions.assertTrue(car.hasAdvancedControl());
     }
+
     @Test
     public void testGetBatteryPercentage() {
         Battery battery = new Battery("disposable");
         Car car = new Car("Red", battery, false);
-        Assertions.assertEquals(100,car.getBattery().getPercentage());
+        Assertions.assertEquals(100, car.getBattery().getPercentage());
         car.getBattery().setPercentage(80);
-        Assertions.assertEquals(80,car.getBattery().getPercentage());
+        Assertions.assertEquals(80, car.getBattery().getPercentage());
     }
 
     @Test
     public void testMoveCar() {
         Battery battery = new Battery("disposable");
         Car car = new Car("Red", battery, false);
-        Assertions.assertEquals("The car moved forth 1 kilometer(s).",car.moveCar("forth", 1));
+        Assertions.assertEquals("The car moved forth 1 kilometer(s).", car.moveCar("forth", 1));
         Assertions.assertTrue(car.isMoving());
         car.stopCar();
         Assertions.assertFalse(car.isMoving());
-        Assertions.assertEquals("The car moved back 2 kilometer(s).",car.moveCar("back", 2));
+        Assertions.assertEquals("The car moved back 2 kilometer(s).", car.moveCar("back", 2));
+    }
+
+    @Test
+    public void testTurnCar() {
+        Battery battery = new Battery("disposable");
+        Car car = new Car("Red", battery, false);
+        Assertions.assertEquals("The car turned left.", car.turnCar("left"));
+        Assertions.assertEquals("left", car.getDirection());
     }
 }
