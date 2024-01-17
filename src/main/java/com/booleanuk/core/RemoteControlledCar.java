b7e3f0dc4a9c14c09912d666321713614502eb2c
Enock97 <enockladu@hotmail.com>
2024-01-17 10:37:10 +0100

    Created classes for exercise 2, and written test for user story 1 (red phase)

--- src/main/java/com/booleanuk/core/RemoteControlledCar.java
+++ src/main/java/com/booleanuk/core/RemoteControlledCar.java
@@ -2,6 +2,9 @@ package com.booleanuk.core;
 
 public class RemoteControlledCar {
 
+    public String carName = "Ferrari";
+    public String carModel = "2020";
+
 
 
 }
--- src/test/java/com/booleanuk/core/RemoteControlledCarTest.java
+++ src/test/java/com/booleanuk/core/RemoteControlledCarTest.java
@@ -1,4 +1,16 @@
 package com.booleanuk.core;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class RemoteControlledCarTest {
+
+    @Test
+    public void testTeturnCarColor() {
+        RemoteControlledCar remoteControlledCar = new RemoteControlledCar();
+
+        String result = remoteControlledCar.chooseCarColor("Blue");
+
+        Assertions.assertEquals("New car color: " + "Blue.", result);
+    }
 }
