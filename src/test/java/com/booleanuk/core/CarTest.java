4b8412797e80e2a39c78c7a63a13592eed9b2251
Vegard Vognstolen <vegard.bruas.vognstolen@no.experis.com>
2024-01-17 09:44:50 +0100

    Setup tests and star testing constructor

---
+++ src/test/java/com/booleanuk/core/CarTest.java
@@ -0,0 +1,46 @@
+package com.booleanuk.core;
+
+import org.junit.jupiter.api.Test;
+
+public class CarTest {
+
+    @Test
+    public void testSetBatteries() {
+        Car car = new Car("Blue", "Rechargable");
+
+    }
+
+    @Test
+    public void testSetRemoteControll() {
+
+    }
+    @Test
+    public void testGetBatteryPercentage() {
+
+    }
+    @Test
+    public void testMoveCarForward() {
+
+    }
+    @Test
+    public void testMoveCarBackwards() {
+
+    }
+    @Test
+    public void testMoveCarRight() {
+
+    }
+    @Test
+    public void testMoveCarLeft() {
+
+    }
+    @Test
+    public void testStopCar() {
+
+    }
+
+
+
+
+
+}
