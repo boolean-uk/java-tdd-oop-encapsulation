60b49f606ea38286bc37335f3657298b74a84709
Enock97 <enockladu@hotmail.com>
2024-01-17 10:45:22 +0100

    Written code for user story 2 that passes its test (green phase)

--- src/main/java/com/booleanuk/core/RemoteControlledCarBattery.java
+++ src/main/java/com/booleanuk/core/RemoteControlledCarBattery.java
@@ -2,4 +2,8 @@ package com.booleanuk.core;
 
 public class RemoteControlledCarBattery {
 
+    public String chooseCarBattery(String batteryType) {
+        return "Chosen car batter: " + "Rechargeable.";
+    }
+
 }
