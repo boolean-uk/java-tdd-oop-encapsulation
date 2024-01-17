b0bb825e1aa7041f94bc7a7b416c1fcda06bad13
Vegard Vognstolen <vegard.bruas.vognstolen@no.experis.com>
2024-01-17 09:50:13 +0100

    Setup class variables and fix constructor to pass test

--- src/main/java/com/booleanuk/core/Car.java
+++ src/main/java/com/booleanuk/core/Car.java
@@ -3,4 +3,17 @@ package com.booleanuk.core;
 public class Car {
 
 
+    private String color;
+    private String batteries;
+    private String remoteControll;
+    private double batteryPercentage;
+    private double positionX;
+    private double positionY;
+
+    public Car(String color, String batteries) {
+        this.color = color;
+        this.batteries = batteries;
+    }
+
+
 }
