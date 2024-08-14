9bfab24c9e0b000a07e792a4d24c0a5cfbf5578b
Iris Flystam <badde00@gmail.com>
2024-08-14 15:04:08 +0200

    REFACTOR: Changed Car to reflect new class diagram.

--- src/main/java/com/booleanuk/core/Car.java
+++ src/main/java/com/booleanuk/core/Car.java
@@ -3,13 +3,15 @@ package com.booleanuk.core;
 public class Car {
     private String color;
     private boolean rechargeableBatteries;
-    private float motorPower;
-    private float turningWheelMotorPower;
+    private int motorPower;
+    private int turningWheelMotorPower;
+    private int location;
 
-    public Car(String color,boolean rechargeableBatteries, float motorPower, float turningWheelMotorPower) {
+    public Car(String color,boolean rechargeableBatteries, int motorPower, int turningWheelMotorPower) {
         this.color = color;
         this.rechargeableBatteries = rechargeableBatteries;
         this.motorPower = motorPower;
         this.turningWheelMotorPower = turningWheelMotorPower;
+        location = 0;
     }
 }
