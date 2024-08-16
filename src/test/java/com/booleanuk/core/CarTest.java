7d4694946c056bcac711a810b8b1baa977e6279c
Thomas Wiik <thomas.wiik@no.experis.com>
2024-08-16 10:31:36 +0200

    Created test for setting color of car.

--- src/test/java/com/booleanuk/core/CarTest.java
+++ src/test/java/com/booleanuk/core/CarTest.java
@@ -1,12 +1,23 @@
 package com.booleanuk.core;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import java.rmi.Remote;
 
 public class CarTest {
     @Test
     public void decideColorOfCar(){
         // I want to be able to decide on the colour of the car.
+        Battery battery = new Battery("disposable");
+        RemoteControl remoteControl = new RemoteControl("simple");
+        Car car = new Car("red", battery, remoteControl);
 
+        Assertions.assertEquals("red", car.color);
+
+        String newCarColor = "green";
+        car.setColor(newCarColor);
+        Assertions.assertEquals(newCarColor, car.color);
     }
 
 
