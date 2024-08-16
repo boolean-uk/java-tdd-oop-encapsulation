ab0b36a1164451a68b304d7bc9d565bd2ab87e83
Thomas Wiik <thomas.wiik@no.experis.com>
2024-08-16 10:27:26 +0200

    Created implementation classes and test classes.

---
+++ src/test/java/com/booleanuk/core/CarTest.java
@@ -0,0 +1,14 @@
+package com.booleanuk.core;
+
+import org.junit.jupiter.api.Test;
+
+public class CarTest {
+    @Test
+    public void decideColorOfCar(){
+        // I want to be able to decide on the colour of the car.
+
+    }
+
+
+
+}
---
+++ src/test/java/com/booleanuk/core/RemoteControlTest.java
@@ -0,0 +1,9 @@
+package com.booleanuk.core;
+
+import org.junit.jupiter.api.Test;
+
+public class RemoteControlTest {
+    @Test
+
+
+}
