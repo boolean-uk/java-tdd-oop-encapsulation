9539922cd53c1b027f5e63f6795a68d7fee87a90
Simon Fredriksson <128689713+SimpFred@users.noreply.github.com>
2024-08-16 10:26:02 +0200

    testCreateACar() pass

--- src/main/java/com/booleanuk/core/Car.java
+++ src/main/java/com/booleanuk/core/Car.java
@@ -1,4 +1,13 @@
 package com.booleanuk.core;
 
 public class Car {
+
+    private final String color;
+    private String battery;
+
+    public Car(String color, String battery) {
+        this.color = color;
+        this.battery = battery;
+    }
+
 }
