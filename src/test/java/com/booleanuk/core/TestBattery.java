3e6bc8a39015107996942c6fd7ec5649a2f4831d
Dag Andreas <daanfofo@gmail.com>
2024-08-16 10:52:43 +0200

    test rechargeable battery func

--- src/test/java/com/booleanuk/core/TestBattery.java
+++ src/test/java/com/booleanuk/core/TestBattery.java
@@ -7,15 +7,33 @@ public class TestBattery {
 
     @Test
     public void testBatteryConstructor(){
-        Battery testbat = new Battery(true, 15, 2);
+        Battery testbat = new Battery(true);
+        Assertions.assertNotNull(testbat);
     }
 
 
     @Test
     public void testUsePower(){
-        Battery testbat = new Battery(true, 15, 2);
+        Battery testbat = new Battery(true);
         testbat.usePower(5);
         Assertions.assertEquals(testbat.getPower(), 95);
+    }
+
+    @Test
+    public void testRechargeBattery(){
+        Battery testbat = new Battery(true);
+        testbat.usePower(15);
+        Assertions.assertEquals(testbat.getPower(), 85);
+        testbat.rechargeBattery(5);
+        Assertions.assertEquals(testbat.getPower(), 90);
+
+
+        testbat = new Battery(false);
+        testbat.usePower(33);
+        Assertions.assertEquals(testbat.getPower(), 67);
+        testbat.rechargeBattery(7);
+        Assertions.assertNotEquals(testbat.getPower(), 74);
+        Assertions.assertEquals(testbat.getPower(), 67);
     }
 
 }
