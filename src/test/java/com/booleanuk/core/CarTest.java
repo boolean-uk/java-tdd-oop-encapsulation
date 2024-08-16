9b5460478c15cbc9126ad7bec3bd72ea23b5a13c
Simon Fredriksson <128689713+SimpFred@users.noreply.github.com>
2024-08-16 10:25:04 +0200

    testCreateACar() fails

--- src/test/java/com/booleanuk/core/CarTest.java
+++ src/test/java/com/booleanuk/core/CarTest.java
@@ -1,6 +1,14 @@
 package com.booleanuk.core;
 
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class CarTest {
 
+    @Test
+    public void testCreateACar() {
+        Car car = new Car("Green", "rechargable");
+        Assertions.assertNotNull(car);
+    }
 }
